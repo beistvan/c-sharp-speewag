bfcb69bcd68a809e09d8b49c66922c51794a8805
Beregszászi István <beistvan@gmail.com>
2023-05-28 06:05:53 +0200

    Ciao

---
+++ ciao.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine($"Ciao, {s}. I'm leaving for better life.");
+    }
+}
