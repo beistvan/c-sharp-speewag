5fae394e8e46d6ed602c9663866c7244002e2633
Beregszászi István <beistvan@gmail.com>
2023-05-28 07:16:33 +0200

    Repeat

---
+++ Repeat.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int i = 0;
+        while(i < 10){
+            Console.WriteLine("Repeat");
+            i++;
+        }
+    }
+}
